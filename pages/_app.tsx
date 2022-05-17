58e1e65996213fe7e8e81d50164f58f73a106426
with-heart <with.heart+git@pm.me>
2022-05-17 15:36:15 -0400

    install and configure Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react'
+import type {AppProps} from 'next/app'
+import {theme} from '../theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ theme.ts
@@ -0,0 +1,7 @@
+import {extendTheme} from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+  },
+})
