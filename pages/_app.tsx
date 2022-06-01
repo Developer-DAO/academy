bd3f9bcb6a43b30feded1b1d9b5ba66d36481c46
Martin Casas <casasmartinignacio@gmail.com>
2022-06-01 16:33:03 -0300

    requested changes - only public provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,15 +13,11 @@ import {
   createClient,
   WagmiConfig,
 } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [
-    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
+  [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
