b52a5c9753b35f28d56904590c5340d42cd0fb35
PBillingsby <philbillingsby@gmail.com>
2022-06-03 10:15:00 -0700

    add lessons index and slug for mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'zh'],
+    defaultLanguage: 'en',
+    fallbackLng: ['en'],
+  },
+  react: { useSuspense: false },
+  reactStrictMode: false
 }
 
 module.exports = nextConfig
