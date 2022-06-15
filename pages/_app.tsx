f0f530ab31ea9b2a57dd6f3597166d545677e2ee
sbelka-1703 <sbelka93@gmail.com>
2022-06-15 11:24:02 -0700

    rebase attempt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import {ChakraProvider} from '@chakra-ui/react'
-import type {AppProps} from 'next/app'
-import {theme} from '../theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { theme } from "../theme";
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
