c3920a9f1f65b28f9e7b42e698495688fffa3f59
with-heart <with.heart+git@pm.me>
2022-06-26 13:09:30 -0400

    add studying states

--- components/PomodoroTimer.test.tsx
+++ components/PomodoroTimer.test.tsx
@@ -1,5 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { minute, second } from '../lib/time'
 import { PomodoroTimer } from './PomodoroTimer'
+
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
+afterEach(() => {
+  jest.runOnlyPendingTimers()
+  jest.useRealTimers()
+})
 
 test('timer starts in the idle state', () => {
   render(<PomodoroTimer />)
@@ -8,4 +19,76 @@ test('timer starts in the idle state', () => {
   expect(
     screen.getByLabelText('time remaining', { selector: 'time' }),
   ).toHaveTextContent('25:00')
+})
+
+test('study session', async () => {
+  const user = userEvent.setup()
+  render(<PomodoroTimer />)
+
+  const timerStatus = screen.getByLabelText('timer status')
+  const timeRemaining = screen.getByLabelText('time remaining')
+
+  // click the Study button
+  user.click(screen.getByRole('button', { name: 'Study' }))
+
+  // wait for Studying state
+  await waitFor(() => expect(timerStatus).toHaveTextContent('Studying'))
+
+  // study session timer begins at 25:00
+  expect(timeRemaining).toHaveTextContent('25:00')
+
+  // timer starts immediately
+  await act(() => {
+    jest.advanceTimersByTime(5 * minute)
+  })
+  await waitFor(() => expect(timeRemaining).toHaveTextContent('20:00'))
+
+  // active study session timer can be paused
+  user.click(screen.getByRole('button', { name: 'Pause' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent('Studying paused'))
+
+  // validate that timer doesn't decrement while paused
+  await act(() => {
+    jest.advanceTimersByTime(5 * second)
+  })
+  await waitFor(() => expect(timeRemaining).toHaveTextContent('20:00'))
+
+  // paused study session timer can be reset back to idle state
+  user.click(screen.getByRole('button', { name: 'Reset' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent(''))
+  expect(timeRemaining).toHaveTextContent('25:00')
+
+  // paused study session timer can be resumed
+  user.click(screen.getByRole('button', { name: 'Study' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent('Studying'))
+  user.click(screen.getByRole('button', { name: 'Pause' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent('Studying paused'))
+  user.click(screen.getByRole('button', { name: 'Resume' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent(/^Studying$/))
+
+  // validate that timer is running again
+  await act(() => {
+    jest.advanceTimersByTime(5 * minute)
+  })
+  await waitFor(() => expect(timeRemaining).toHaveTextContent('20:00'))
+
+  // active study session timer can be reset back to idle state
+  user.click(screen.getByRole('button', { name: 'Reset' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent(''))
+  expect(timeRemaining).toHaveTextContent('25:00')
+
+  // return to Studying state
+  user.click(screen.getByRole('button', { name: 'Study' }))
+  await waitFor(() => expect(timerStatus).toHaveTextContent('Studying'))
+
+  // active study session timer counts down to 00:00
+  await act(() => {
+    jest.advanceTimersByTime(25 * minute)
+  })
+  await waitFor(() =>
+    expect(timerStatus).toHaveTextContent('Studying complete'),
+  )
+  expect(timeRemaining).toHaveTextContent('00:00')
+
+  // TODO: empty study session timer can begin a break session
 })
--- components/PomodoroTimer.tsx
+++ components/PomodoroTimer.tsx
@@ -1,8 +1,103 @@
+import { useInterval } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { formatTime, minute, second } from '../lib/time'
+
+export enum TimerStatus {
+  Idle = 'idle',
+  Studying = 'studying',
+  StudyingPaused = 'studying-paused',
+  StudyingComplete = 'studying-complete',
+}
+
+const Status = ({ children }: { children?: string }) => (
+  <div aria-label="timer status">{children}</div>
+)
+
+const TimeRemaining = ({ time }: { time: number }) => (
+  <time aria-label="time remaining">{formatTime(time)}</time>
+)
+
 export const PomodoroTimer = () => {
-  return (
-    <div>
-      <div aria-label="timer status" />
-      <time aria-label="time remaining">25:00</time>
-    </div>
-  )
+  const [status, setStatus] = useState(TimerStatus.Idle)
+  const [timeRemaining, setTimeRemaining] = useState(25 * minute)
+  const [isRunning, setIsRunning] = useState(false)
+
+  const onStudy = () => {
+    setStatus(TimerStatus.Studying)
+    setIsRunning(true)
+  }
+
+  const onPause = () => {
+    setStatus(TimerStatus.StudyingPaused)
+    setIsRunning(false)
+  }
+
+  const onResume = () => {
+    setStatus(TimerStatus.Studying)
+    setIsRunning(true)
+  }
+
+  const onReset = () => {
+    setStatus(TimerStatus.Idle)
+    setTimeRemaining(25 * minute)
+    setIsRunning(false)
+  }
+
+  const tick = () => {
+    setTimeRemaining((timeRemaining) => {
+      const remaining = timeRemaining - 1 * second
+      return remaining < 0 ? 0 : remaining
+    })
+  }
+
+  useInterval(tick, isRunning ? 1 * second : null)
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setStatus(TimerStatus.StudyingComplete)
+      setIsRunning(false)
+    }
+  }, [timeRemaining])
+
+  switch (status) {
+    case TimerStatus.Idle:
+      return (
+        <div>
+          <Status />
+          <TimeRemaining time={timeRemaining} />
+          <button onClick={onStudy}>Study</button>
+        </div>
+      )
+    case TimerStatus.Studying:
+      return (
+        <div>
+          <Status>Studying</Status>
+          <TimeRemaining time={timeRemaining} />
+          <button onClick={onPause}>Pause</button>
+          <button onClick={onReset}>Reset</button>
+        </div>
+      )
+    case TimerStatus.StudyingPaused:
+      return (
+        <div>
+          <Status>Studying paused</Status>
+          <TimeRemaining time={timeRemaining} />
+          <button onClick={onResume}>Resume</button>
+          <button onClick={onReset}>Reset</button>
+        </div>
+      )
+    case TimerStatus.StudyingComplete:
+      return (
+        <div>
+          <Status>Studying complete</Status>
+          <TimeRemaining time={timeRemaining} />
+          <button onClick={onResume}>Resume</button>
+          <button onClick={onReset}>Reset</button>
+        </div>
+      )
+    default:
+      // throw if status is not handled, helpful for development
+      const exhaustiveCheck: never = status
+      throw new Error(`Unhandled status: ${exhaustiveCheck}`)
+  }
 }
