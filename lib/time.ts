a3ba444eb6e38e4d16912c523808539a4de45fac
with-heart <with.heart+git@pm.me>
2022-06-26 11:26:18 -0400

    add time library

---
+++ lib/time.ts
@@ -0,0 +1,24 @@
+export const millisecond = 1000
+
+export const second = millisecond * 60
+
+export const minute = second * 60
+
+/**
+ * Converts milliseconds into the 'HH:MM' format
+ *
+ * @example
+ * formatTime(1 * second) // '00:01'
+ * formatTime(59 * second) // '00:59'
+ * formatTime(1 * minute) // '01:00'
+ * formatTime(1 * minute + 1 * second) // '01:01'
+ * formatTime(10 * minute) // '10:00'
+ * formatTime(10 * minute + 1 * second) // '10:01'
+ */
+export function formatTime(ms: number) {
+  const minutes = Math.floor(ms / minute)
+  const seconds = Math.floor((ms % minute) / second)
+  return `${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}`
+}
