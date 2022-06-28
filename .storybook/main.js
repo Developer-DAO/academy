0d420147a7b00346b89a4d6bbcaa29200466883a
with-heart <with.heart+git@pm.me>
2022-06-28 10:10:02 -0400

    add @chakra-ui/storybook-addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@chakra-ui/storybook-addon',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
