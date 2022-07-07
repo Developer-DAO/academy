c5fc1aa794612d3091dd8083f45283a536d40b36
Ropats <rohitcpathare@gmail.com>
2022-07-07 22:59:24 +0530

    Lessons index page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,28 +1,44 @@
 import React from 'react'
-import { Image, Box, Flex, Heading, Button, Spacer, Link, IconButton } from '@chakra-ui/react'
+import {
+  Image,
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Spacer,
+  Link,
+  IconButton,
+} from '@chakra-ui/react'
 import HamburgerIcon from './HamburgerIcon'
-
 
 function NavBar() {
   return (
-    <Box pl='235px' pr='235px' >
-      <Flex justify='center' alignItems='center'>
+    <Box pl="235px" pr="235px">
+      <Flex justify="center" alignItems="center">
         <Box my={-20}>
-          <Image boxSize={{ base: '120px', md: '300px' }} src="/logo.svg" />
+          <Image
+            boxSize={{ base: '120px', md: '300px' }}
+            src="/public/assets/logo.svg"
+          />
         </Box>
         <Spacer />
         <Box>
-          <Flex alignItems='center'>
-            <Link pr='9' color="yellow.300">JOIN THE COMMUNITY</Link>
-            <Button colorScheme='gray' variant='solid'>Connect Wallet</Button>
-            <IconButton aria-label='Search database' variant='ghost' icon={<HamburgerIcon />} />
-
+          <Flex alignItems="center">
+            <Link pr="9" color="yellow.300">
+              JOIN THE COMMUNITY
+            </Link>
+            <Button colorScheme="gray" variant="solid">
+              Connect Wallet
+            </Button>
+            <IconButton
+              aria-label="Search database"
+              variant="ghost"
+              icon={<HamburgerIcon />}
+            />
           </Flex>
         </Box>
-
       </Flex>
     </Box>
-
   )
 }
 
---
+++ pages/lessons/[slug].tsx
@@ -0,0 +1,57 @@
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { useTranslation } from 'next-i18next'
+
+interface LessonProps {
+  frontMatter: {
+    i18n: string
+  }
+  mdxSource: MDXRemoteSerializeResult
+}
+
+const Lesson: React.FC<LessonProps> = ({
+  frontMatter: { i18n },
+  mdxSource,
+}) => {
+  const { t } = useTranslation(i18n)
+  return (
+    <div>
+      <MDXRemote {...mdxSource} components={{ SyntaxHighlighter }} />
+    </div>
+  )
+}
+
+export default Lesson
+
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync(path.join('lessons'))
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.mdx', ''),
+    },
+  }))
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async ({ params: { slug } }: any) => {
+  const markdownWithMeta = fs.readFileSync(
+    path.join('lessons', slug + '.mdx'),
+    'utf-8',
+  )
+  const { data: frontMatter, content } = matter(markdownWithMeta)
+  const mdxSource = await serialize(content)
+  return {
+    props: {
+      frontMatter,
+      slug,
+      mdxSource,
+    },
+  }
+}
