e6548b0203d5c8370be250343fbceb39ac19966a
Brian Gershon <briangershon@users.noreply.github.com>
2022-07-21 17:28:00 -0700

    Add Hamburger menu for mobile.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,11 +5,14 @@ import {
   Image,
   Box,
   Flex,
-  Heading,
   Button,
   Spacer,
   Link,
   IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from '@chakra-ui/react'
 import HamburgerIcon from './HamburgerIcon'
 import SchoolOfCodeLogo from './SchoolOfCodeLogo'
@@ -26,7 +29,52 @@ function NavBar() {
         </Link>
         <Spacer />
         <Box>
-          <Flex alignItems="center">
+          {/*
+                MOBILE / HAMBURGER MENU
+          */}
+          <Flex alignItems="center" display={{ base: 'block', md: 'none' }}>
+            <Menu autoSelect={false}>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+              />
+              <MenuList backgroundColor={'soc.vividViolet'}>
+                <MenuItem>
+                  <NextLink href="/" passHref>
+                    <Link
+                      variant={
+                        router.pathname === '/'
+                          ? 'top-navigation-active'
+                          : 'top-navigation'
+                      }
+                    >
+                      Home
+                    </Link>
+                  </NextLink>
+                </MenuItem>
+                <MenuItem>
+                  <NextLink href="/lessons" passHref>
+                    <Link
+                      variant={
+                        router.pathname.startsWith('/lessons')
+                          ? 'top-navigation-active'
+                          : 'top-navigation'
+                      }
+                    >
+                      Tracks
+                    </Link>
+                  </NextLink>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+
+          {/*
+                DESKTOP VERSION
+          */}
+          <Flex alignItems="center" display={{ base: 'none', md: 'block' }}>
             <NextLink href="/" passHref>
               <Link
                 variant={
@@ -54,11 +102,6 @@ function NavBar() {
             {/* <Button colorScheme="gray" variant="solid">
               <ConnectButton chainStatus="icon" showBalance={false} />
             </Button> */}
-            {/* <IconButton
-              aria-label="Search database"
-              variant="ghost"
-              icon={<HamburgerIcon />}
-            /> */}
           </Flex>
         </Box>
       </Flex>
