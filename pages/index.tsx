7aa1d7d19c083a1c60b650a943fbd61cd4e8d6b7
Brian Gershon <briangershon@users.noreply.github.com>
2022-09-18 22:54:45 -0700

    Move NextSeo metadata into app as default for all pages.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,10 @@
 import { Box } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Hero from '../components/Hero'
-import { NextSeo } from 'next-seo'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <NextSeo
-        title="Developer DAO Academy"
-        description="Academy is an open-source education platform created by the
-        Developer DAO."
-        openGraph={{
-          images: [
-            {
-              url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/landing-page-screenshot.png`,
-              width: 800,
-              height: 600,
-              alt: 'Developer DAO Academy',
-              type: 'image/png',
-            },
-          ],
-        }}
-        additionalLinkTags={[
-          {
-            rel: 'icon',
-            href: '/favicon.ico',
-          },
-        ]}
-      />
-
       <main>
         <Box as="main">
           <Hero />
