85b3c32324c4946569559fe96e100327f0d9132f
AlanRacciatti <alanracciatti1220@gmail.com>
2023-01-12 22:17:30 -0300

    feat: add question component

--- components/mdx/Components.tsx
+++ components/mdx/Components.tsx
@@ -4,6 +4,7 @@ import dracula from 'react-syntax-highlighter/dist/cjs/styles/prism/dracula'
 import { CopyToClipboard } from '../CopyToClipboard'
 import SideDrawer from './SideDrawer'
 import Quiz from './Quiz'
+import Question from './Question'
 import Callout from './Callout'
 
 const Components = {
@@ -43,6 +44,7 @@ const Components = {
   SideDrawer,
   Callout,
   Quiz,
+  Question,
 }
 
 export default Components
