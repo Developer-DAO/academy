256953b359521e34bbd4245c29fd7526172d5122
Brian Gershon <briangershon@users.noreply.github.com>
2023-02-24 22:28:07 -0800

    Remove extra <a> to fix Next.js warning

--- components/ContentBanner.tsx
+++ components/ContentBanner.tsx
@@ -28,27 +28,21 @@ export const ContentBanner: React.FC<LessonProps> = (props: LessonProps) => {
   return (
     <>
       <Link key={slug} href={`/lessons/${path}/${slug}`} passHref>
-        <a>
-          <Flex direction="column" bg="gray.800" p={5} rounded={5}>
-            <Flex
-              direction="row"
-              justify="space-between"
-              align="center"
-              // bg="#35363A"
-            >
-              <Flex direction="column" justifyContent="center">
-                <Flex color="#FFD500">
-                  <Heading
-                    as="h3"
-                    apply="mdx.h3"
-                    fontSize={[14, 14, 16]}
-                    my={2}
-                  >
-                    {title}
-                  </Heading>
-                </Flex>
+        <Flex direction="column" bg="gray.800" p={5} rounded={5}>
+          <Flex
+            direction="row"
+            justify="space-between"
+            align="center"
+            // bg="#35363A"
+          >
+            <Flex direction="column" justifyContent="center">
+              <Flex color="#FFD500">
+                <Heading as="h3" apply="mdx.h3" fontSize={[14, 14, 16]} my={2}>
+                  {title}
+                </Heading>
               </Flex>
-              {/* <Flex
+            </Flex>
+            {/* <Flex
                 bg="#00000f"
                 fontSize={[0, 6, 8, 10]}
                 px={2}
@@ -58,32 +52,31 @@ export const ContentBanner: React.FC<LessonProps> = (props: LessonProps) => {
               >
                 Lesson {idx + 1}
               </Flex> */}
-              <Wrap ml={20}>
-                {icons &&
-                  icons.map((icon: string, idx: number) => (
-                    <WrapItem
-                      key={idx}
-                      bg="#FFFFFF"
-                      p={1.5}
-                      rounded={'50%'}
-                      mx="auto"
-                      title={icon}
-                    >
-                      <Image
-                        src={`/assets/${icon}.png`}
-                        width={16}
-                        height={16}
-                        alt={icon}
-                      />
-                    </WrapItem>
-                  ))}
-              </Wrap>
-            </Flex>
-            <Flex fontSize={[10, 10, 12, 14]} pt={4}>
-              {description}
-            </Flex>
+            <Wrap ml={20}>
+              {icons &&
+                icons.map((icon: string, idx: number) => (
+                  <WrapItem
+                    key={idx}
+                    bg="#FFFFFF"
+                    p={1.5}
+                    rounded={'50%'}
+                    mx="auto"
+                    title={icon}
+                  >
+                    <Image
+                      src={`/assets/${icon}.png`}
+                      width={16}
+                      height={16}
+                      alt={icon}
+                    />
+                  </WrapItem>
+                ))}
+            </Wrap>
           </Flex>
-        </a>
+          <Flex fontSize={[10, 10, 12, 14]} pt={4}>
+            {description}
+          </Flex>
+        </Flex>
       </Link>
     </>
   )
