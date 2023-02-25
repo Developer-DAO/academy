2d695adae3cdb4f32bd7fe34bb552f0dfbcc8ae6
Brian Gershon <briangershon@users.noreply.github.com>
2023-02-24 23:59:20 -0800

    Remove storybook -- unused and was broken

--- components/SchoolOfCodeLogo.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SchoolOfCodeLogo from '@components/SchoolOfCodeLogo'
-
-export default {
-  component: SchoolOfCodeLogo,
-  title: 'SchoolOfCodeLogo',
-}
-
-export const Default = () => <SchoolOfCodeLogo autoStart={true} loop={true} />
