653896992c481d28291683bb102a6676bdd1b4da
Brian Gershon <briangershon@users.noreply.github.com>
2023-02-24 23:28:30 -0800

    Fix test config (though tests still broken) and also add moduleNameMappers.

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,6 +8,12 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@pages/(.*)$': '<rootDir>/pages/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
