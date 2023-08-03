3f28d1390e0483103577f509a6987420b170466b
Marcos Iglesias <markkos89@gmail.com>
2023-08-03 12:04:14 -0300

    feat: configuring next config eslint for mdx

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'))
 
-import remarkFrontmatter from "remark-frontmatter";
-import nextMDX from "@next/mdx";
+import remarkFrontmatter from 'remark-frontmatter'
+import nextMDX from '@next/mdx'
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -13,9 +13,9 @@ const withMDX = nextMDX({
     remarkPlugins: [remarkFrontmatter],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
+    providerImportSource: '@mdx-js/react',
   },
-});
+})
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -23,10 +23,10 @@ const config = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "www.developerdao.com",
-        port: "",
-        pathname: "D_D_logo-dark.svg",
+        protocol: 'https',
+        hostname: 'www.developerdao.com',
+        port: '',
+        pathname: 'D_D_logo-dark.svg',
       },
     ],
   },
@@ -37,11 +37,14 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-};
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  eslint: {
+    dirs: ['src', 'utils'], // Only run ESLint on the 'src' and 'utils' directories during production builds (next build)
+  },
+}
 
-export default withMDX(config);
+export default withMDX(config)
