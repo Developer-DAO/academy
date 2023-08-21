25c40712553d1066935f05b4034774c7bfd1a8d5
okhaimie-dev <oktonystark8@gmail.com>
2023-08-21 16:01:19 -0500

    chore: fix contributor-page fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         px="5%"
         mx={{ base: '2rem', md: '6rem', lg: '10rem' }}
         as="main"
+        minH={{ base: 'calc(75vh - 3.5rem)' }}
       >
         {children}
       </Box>
